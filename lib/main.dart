0baeffd987fabde67d82739f02d1ee9ab7cd15a7
Max Lantas <mnxndev@outlook.com>
2022-02-05 17:55:36 -0800

    use system theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'package:hiit/theme.dart';
 import 'package:hiit/hiit.dart';
+import 'package:hiit/theme.dart' as theme;
 import 'package:hiit/themed_timerpicker.dart';
 import 'package:hiit/themed_numberpicker.dart';
 
@@ -16,11 +14,16 @@ void main() async {
 
   preferences = await SharedPreferences.getInstance();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(const MaterialApp(title: 'HIIT Timer', home: Home()));
+  runApp(MaterialApp(
+    title: 'HIIT Timer',
+    home: const Home(),
+    theme: theme.light(),
+    darkTheme: theme.dark(),
+    themeMode: ThemeMode.system,
+  ));
 }
 
 class Default {
-  static const bool darkmode = false;
   static const int warmup = 120;
   static const int work = 30;
   static const int rest = 90;
@@ -44,13 +47,13 @@ class HomeState extends State<Home> with SingleTickerProviderStateMixin {
     switch (timer.current.kind) {
       case IntervalKind.warmUp:
       case IntervalKind.coolDown:
-        return AppTheme.coolDownColor;
+        return theme.coolDownColor;
       case IntervalKind.work:
-        return AppTheme.workColor;
+        return theme.workColor;
       case IntervalKind.rest:
-        return AppTheme.restColor;
+        return theme.restColor;
       default:
-        return AppTheme.defaultColor;
+        return theme.defaultColor;
     }
   }
 
@@ -83,217 +86,190 @@ class HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    ThemeData theme;
-    if (preferences.getBool("darkmode") ?? Default.darkmode) {
-      theme = AppTheme.dark(context);
-    } else {
-      theme = AppTheme.light(context);
-    }
-
-    return Theme(
-      data: theme,
-      child: Scaffold(
-        key: scaffoldKey,
-        backgroundColor: timerColor(),
-        appBar: AppBar(
-          title: Text(timer.titleText),
-          centerTitle: true,
-          leading: Container(), // hide drawer hamburger menu
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Center(
-                  child: Container(
-                    decoration: BoxDecoration(color: theme.primaryColor, shape: BoxShape.circle),
-                    width: MediaQuery.of(context).size.shortestSide * 0.85,
-                    height: MediaQuery.of(context).size.shortestSide * 0.85,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(timer.repText, style: theme.textTheme.headline3),
-                        Text(timer.current.remainingText, style: theme.textTheme.headline1),
-                        Text(timer.subtext, style: theme.textTheme.headline3),
-                      ],
-                    ),
-                  ),
-                ),
-                Center(
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.shortestSide * 0.8,
-                    height: MediaQuery.of(context).size.shortestSide * 0.8,
-                    child: CircularProgressIndicator(
-                      value: _controller.value,
-                      backgroundColor: Color.lerp(
-                        theme.primaryColor,
-                        theme.colorScheme.secondary,
-                        0.25,
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-          child: Icon(timer.isRunning ? Icons.pause : Icons.play_arrow),
-          foregroundColor: theme.primaryColor,
-          shape: StadiumBorder(side: BorderSide(color: theme.primaryColor, width: 3)),
-          tooltip: timer.isRunning ? "Pause" : "Play",
-          onPressed: () => setState(timer.playpause),
-        ),
-        bottomNavigationBar: BottomAppBar(
-            shape: const CircularNotchedRectangle(),
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height * 0.125,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: timerColor(),
+      appBar: AppBar(
+        title: Text(timer.titleText),
+        centerTitle: true,
+        leading: Container(), // hide drawer hamburger menu
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(color: Theme.of(context).primaryColor, shape: BoxShape.circle),
+                  width: MediaQuery.of(context).size.shortestSide * 0.85,
+                  height: MediaQuery.of(context).size.shortestSide * 0.85,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      IconButton(
-                        icon: const Icon(Icons.settings),
-                        tooltip: "Settings",
-                        onPressed: () {
-                          scaffoldKey.currentState?.openDrawer();
-                          timer.isRunning = false;
-                        },
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.replay),
-                        tooltip: "Restart",
-                        onPressed: () => setState(timer.restart),
-                      ),
+                      Text(timer.repText, style: Theme.of(context).textTheme.headline3),
+                      Text(timer.current.remainingText, style: Theme.of(context).textTheme.headline1),
+                      Text(timer.subtext, style: Theme.of(context).textTheme.headline3),
                     ],
                   ),
-                ],
+                ),
               ),
-            )),
-        drawer: Drawer(
-          child: Container(
-            decoration: BoxDecoration(
-              color: theme.primaryColor,
-              border: Border(right: BorderSide(color: theme.colorScheme.secondary, width: 2)),
-            ),
-            child: ListView(
+              Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.shortestSide * 0.8,
+                  height: MediaQuery.of(context).size.shortestSide * 0.8,
+                  child: CircularProgressIndicator(
+                    value: _controller.value,
+                    backgroundColor: Color.lerp(
+                      Theme.of(context).primaryColor,
+                      Theme.of(context).colorScheme.secondary,
+                      0.25,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(timer.isRunning ? Icons.pause : Icons.play_arrow),
+        foregroundColor: Theme.of(context).primaryColor,
+        shape: StadiumBorder(side: BorderSide(color: Theme.of(context).primaryColor, width: 3)),
+        tooltip: timer.isRunning ? "Pause" : "Play",
+        onPressed: () => setState(timer.playpause),
+      ),
+      bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.125,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Container(
-                  color: theme.colorScheme.secondary,
-                  padding: const EdgeInsets.all(25),
-                  child: ListTile(
-                    leading: Icon(Icons.settings, color: theme.primaryColor),
-                    dense: true,
-                    title: Text('Settings', textScaleFactor: 1.5, style: TextStyle(color: theme.primaryColor)),
-                  ),
-                ),
-                const ListTile(
-                  leading: Icon(Icons.style),
-                  dense: true,
-                  title: Text('Theme', textScaleFactor: 1.25),
-                ),
-                RadioListTile(
-                  title: const Text("Light Theme"),
-                  groupValue: false,
-                  value: preferences.getBool("darkmode") ?? Default.darkmode,
-                  onChanged: (_) => _do(preferences.setBool("darkmode", false)),
-                ),
-                RadioListTile(
-                  title: const Text("Dark Theme"),
-                  groupValue: true,
-                  value: preferences.getBool("darkmode") ?? Default.darkmode,
-                  onChanged: (_) => _do(preferences.setBool("darkmode", true)),
-                ),
-                Divider(color: theme.colorScheme.secondary),
-                const ListTile(
-                  leading: Icon(Icons.timer),
-                  dense: true,
-                  title: Text('Timer', textScaleFactor: 1.25),
-                ),
-                TimerInput(
-                  title: "Warm-Up Time",
-                  backgroundColor: theme.primaryColor,
-                  accentColor: theme.colorScheme.secondary,
-                  value: Duration(seconds: preferences.getInt("warmup") ?? Default.warmup),
-                  onConfirm: (duration) {
-                    _do(preferences.setInt("warmup", duration.inSeconds));
-                    timer.warmUpTime = duration;
-                    timer.restart();
-                  },
-                ),
-                TimerInput(
-                  title: "Work Time",
-                  backgroundColor: theme.primaryColor,
-                  accentColor: theme.colorScheme.secondary,
-                  value: Duration(seconds: preferences.getInt("work") ?? Default.work),
-                  onConfirm: (duration) {
-                    _do(preferences.setInt("work", duration.inSeconds));
-                    timer.workTime = duration;
-                    timer.restart();
-                  },
-                ),
-                TimerInput(
-                  title: "Rest Time",
-                  backgroundColor: theme.primaryColor,
-                  accentColor: theme.colorScheme.secondary,
-                  value: Duration(seconds: preferences.getInt("rest") ?? Default.rest),
-                  onConfirm: (duration) {
-                    _do(preferences.setInt("rest", duration.inSeconds));
-                    timer.restTime = duration;
-                    timer.restart();
-                  },
-                ),
-                TimerInput(
-                  title: "Cool-Down Time",
-                  backgroundColor: theme.primaryColor,
-                  accentColor: theme.colorScheme.secondary,
-                  value: Duration(seconds: preferences.getInt("cooldown") ?? Default.cooldown),
-                  onConfirm: (duration) {
-                    _do(preferences.setInt("cooldown", duration.inSeconds));
-                    timer.coolDownTime = duration;
-                    timer.restart();
-                  },
-                ),
-                NumberInput(
-                  title: "Number of Sets",
-                  label: "Sets",
-                  backgroundColor: theme.primaryColor,
-                  accentColor: theme.colorScheme.secondary,
-                  value: preferences.getInt("sets") ?? Default.sets,
-                  onConfirm: (value) {
-                    _do(preferences.setInt("sets", value));
-                    timer.sets = value;
-                    timer.restart();
-                  },
-                ),
-                Divider(color: theme.colorScheme.secondary),
-                Center(
-                  child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(side: BorderSide(color: theme.colorScheme.secondary, width: 1)),
-                    child: const Text("Default Settings"),
-                    onPressed: () {
-                      _do(preferences.setBool("darkmode", Default.darkmode));
-                      _do(preferences.setInt("warmup", Default.warmup));
-                      _do(preferences.setInt("work", Default.work));
-                      _do(preferences.setInt("rest", Default.rest));
-                      _do(preferences.setInt("cooldown", Default.cooldown));
-                      _do(preferences.setInt("sets", Default.sets));
-                      timer.warmUpTime = const Duration(seconds: Default.warmup);
-                      timer.workTime = const Duration(seconds: Default.work);
-                      timer.restTime = const Duration(seconds: Default.rest);
-                      timer.coolDownTime = const Duration(seconds: Default.cooldown);
-                      timer.sets = Default.sets;
-                      timer.restart();
-                    },
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.settings),
+                      tooltip: "Settings",
+                      onPressed: () {
+                        scaffoldKey.currentState?.openDrawer();
+                        timer.isRunning = false;
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.replay),
+                      tooltip: "Restart",
+                      onPressed: () => setState(timer.restart),
+                    ),
+                  ],
                 ),
               ],
             ),
+          )),
+      drawer: Drawer(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            border: Border(right: BorderSide(color: Theme.of(context).colorScheme.secondary, width: 2)),
+          ),
+          child: ListView(
+            children: [
+              Container(
+                color: Theme.of(context).colorScheme.secondary,
+                padding: const EdgeInsets.all(25),
+                child: ListTile(
+                  leading: Icon(Icons.settings, color: Theme.of(context).primaryColor),
+                  dense: true,
+                  title:
+                      Text('Settings', textScaleFactor: 1.5, style: TextStyle(color: Theme.of(context).primaryColor)),
+                ),
+              ),
+              const ListTile(
+                leading: Icon(Icons.timer),
+                dense: true,
+                title: Text('Timer', textScaleFactor: 1.25),
+              ),
+              TimerInput(
+                title: "Warm-Up Time",
+                backgroundColor: Theme.of(context).primaryColor,
+                accentColor: Theme.of(context).colorScheme.secondary,
+                value: Duration(seconds: preferences.getInt("warmup") ?? Default.warmup),
+                onConfirm: (duration) {
+                  _do(preferences.setInt("warmup", duration.inSeconds));
+                  timer.warmUpTime = duration;
+                  timer.restart();
+                },
+              ),
+              TimerInput(
+                title: "Work Time",
+                backgroundColor: Theme.of(context).primaryColor,
+                accentColor: Theme.of(context).colorScheme.secondary,
+                value: Duration(seconds: preferences.getInt("work") ?? Default.work),
+                onConfirm: (duration) {
+                  _do(preferences.setInt("work", duration.inSeconds));
+                  timer.workTime = duration;
+                  timer.restart();
+                },
+              ),
+              TimerInput(
+                title: "Rest Time",
+                backgroundColor: Theme.of(context).primaryColor,
+                accentColor: Theme.of(context).colorScheme.secondary,
+                value: Duration(seconds: preferences.getInt("rest") ?? Default.rest),
+                onConfirm: (duration) {
+                  _do(preferences.setInt("rest", duration.inSeconds));
+                  timer.restTime = duration;
+                  timer.restart();
+                },
+              ),
+              TimerInput(
+                title: "Cool-Down Time",
+                backgroundColor: Theme.of(context).primaryColor,
+                accentColor: Theme.of(context).colorScheme.secondary,
+                value: Duration(seconds: preferences.getInt("cooldown") ?? Default.cooldown),
+                onConfirm: (duration) {
+                  _do(preferences.setInt("cooldown", duration.inSeconds));
+                  timer.coolDownTime = duration;
+                  timer.restart();
+                },
+              ),
+              NumberInput(
+                title: "Number of Sets",
+                label: "Sets",
+                backgroundColor: Theme.of(context).primaryColor,
+                accentColor: Theme.of(context).colorScheme.secondary,
+                value: preferences.getInt("sets") ?? Default.sets,
+                onConfirm: (value) {
+                  _do(preferences.setInt("sets", value));
+                  timer.sets = value;
+                  timer.restart();
+                },
+              ),
+              Divider(color: Theme.of(context).colorScheme.secondary),
+              Center(
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: Theme.of(context).colorScheme.secondary, width: 1)),
+                  child: const Text("Default Settings"),
+                  onPressed: () {
+                    _do(preferences.setInt("warmup", Default.warmup));
+                    _do(preferences.setInt("work", Default.work));
+                    _do(preferences.setInt("rest", Default.rest));
+                    _do(preferences.setInt("cooldown", Default.cooldown));
+                    _do(preferences.setInt("sets", Default.sets));
+                    timer.warmUpTime = const Duration(seconds: Default.warmup);
+                    timer.workTime = const Duration(seconds: Default.work);
+                    timer.restTime = const Duration(seconds: Default.rest);
+                    timer.coolDownTime = const Duration(seconds: Default.cooldown);
+                    timer.sets = Default.sets;
+                    timer.restart();
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/theme.dart
+++ lib/theme.dart
@@ -10,72 +10,70 @@ String? defaultFont() {
   }
 }
 
-class AppTheme {
-  static const workColor = Color.fromRGBO(231, 72, 86, 1);
-  static const restColor = Color.fromRGBO(0, 204, 106, 1);
-  static const coolDownColor = Color.fromRGBO(99, 108, 122, 1);
-  static const defaultColor = Color.fromRGBO(45, 51, 59, 1);
+const workColor = Color.fromRGBO(231, 72, 86, 1);
+const restColor = Color.fromRGBO(0, 204, 106, 1);
+const coolDownColor = Color.fromRGBO(99, 108, 122, 1);
+const defaultColor = Color.fromRGBO(45, 51, 59, 1);
 
-  static ThemeData dark(BuildContext context) {
-    final TextTheme defaultTheme = Typography().black;
-    return ThemeData(
-      fontFamily: defaultFont(),
-      brightness: Brightness.dark,
-      primaryColor: Colors.black,
-      bottomAppBarColor: Colors.black,
-      dialogBackgroundColor: Colors.black,
-      iconTheme: const IconThemeData(color: CupertinoColors.extraLightBackgroundGray),
-      appBarTheme: const AppBarTheme(
-        backgroundColor: Colors.black,
-        toolbarTextStyle: TextStyle(
-          color: CupertinoColors.extraLightBackgroundGray,
-          fontSize: 30,
-          letterSpacing: 4,
-        ),
-        titleTextStyle: TextStyle(
-          color: CupertinoColors.extraLightBackgroundGray,
-          fontSize: 30,
-          letterSpacing: 4,
-        ),
+ThemeData dark() {
+  final TextTheme defaultTheme = Typography().black;
+  return ThemeData(
+    fontFamily: defaultFont(),
+    brightness: Brightness.dark,
+    primaryColor: Colors.black,
+    bottomAppBarColor: Colors.black,
+    dialogBackgroundColor: Colors.black,
+    iconTheme: const IconThemeData(color: CupertinoColors.extraLightBackgroundGray),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.black,
+      toolbarTextStyle: TextStyle(
+        color: CupertinoColors.extraLightBackgroundGray,
+        fontSize: 30,
+        letterSpacing: 4,
       ),
-      textTheme: TextTheme(
-        headline4: defaultTheme.headline4?.copyWith(color: CupertinoColors.inactiveGray, fontSize: 30),
-        headline3: defaultTheme.headline3?.copyWith(color: CupertinoColors.inactiveGray, fontSize: 40),
-        headline1: defaultTheme.headline1?.copyWith(color: CupertinoColors.extraLightBackgroundGray, fontSize: 100),
+      titleTextStyle: TextStyle(
+        color: CupertinoColors.extraLightBackgroundGray,
+        fontSize: 30,
+        letterSpacing: 4,
       ),
-      colorScheme: ColorScheme.fromSwatch()
-          .copyWith(secondary: CupertinoColors.extraLightBackgroundGray, brightness: Brightness.dark),
-    );
-  }
+    ),
+    textTheme: TextTheme(
+      headline4: defaultTheme.headline4?.copyWith(color: CupertinoColors.inactiveGray, fontSize: 30),
+      headline3: defaultTheme.headline3?.copyWith(color: CupertinoColors.inactiveGray, fontSize: 40),
+      headline1: defaultTheme.headline1?.copyWith(color: CupertinoColors.extraLightBackgroundGray, fontSize: 100),
+    ),
+    colorScheme: ColorScheme.fromSwatch()
+        .copyWith(secondary: CupertinoColors.extraLightBackgroundGray, brightness: Brightness.dark),
+  );
+}
 
-  static ThemeData light(BuildContext context) {
-    final TextTheme defaultTheme = Typography().white;
-    return ThemeData(
-      fontFamily: defaultFont(),
-      brightness: Brightness.light,
-      primaryColor: CupertinoColors.extraLightBackgroundGray,
-      bottomAppBarColor: CupertinoColors.extraLightBackgroundGray,
-      dialogBackgroundColor: CupertinoColors.extraLightBackgroundGray,
-      iconTheme: const IconThemeData(color: Colors.black),
-      appBarTheme: const AppBarTheme(
-        backgroundColor: CupertinoColors.extraLightBackgroundGray,
-        toolbarTextStyle: TextStyle(
-          color: Colors.black,
-          fontSize: 30,
-          letterSpacing: 4,
-        ),
-        titleTextStyle: TextStyle(
-          color: Colors.black,
-          fontSize: 30,
-          letterSpacing: 4,
-        ),
+ThemeData light() {
+  final TextTheme defaultTheme = Typography().white;
+  return ThemeData(
+    fontFamily: defaultFont(),
+    brightness: Brightness.light,
+    primaryColor: CupertinoColors.extraLightBackgroundGray,
+    bottomAppBarColor: CupertinoColors.extraLightBackgroundGray,
+    dialogBackgroundColor: CupertinoColors.extraLightBackgroundGray,
+    iconTheme: const IconThemeData(color: Colors.black),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: CupertinoColors.extraLightBackgroundGray,
+      toolbarTextStyle: TextStyle(
+        color: Colors.black,
+        fontSize: 30,
+        letterSpacing: 4,
       ),
-      textTheme: TextTheme(
-        headline4: defaultTheme.headline4?.copyWith(color: CupertinoColors.inactiveGray, fontSize: 30),
-        headline3: defaultTheme.headline3?.copyWith(color: CupertinoColors.inactiveGray, fontSize: 40),
-        headline1: defaultTheme.headline1?.copyWith(color: Colors.black, fontSize: 100),
+      titleTextStyle: TextStyle(
+        color: Colors.black,
+        fontSize: 30,
+        letterSpacing: 4,
       ),
-      colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.black),
-    );
-  }
+    ),
+    textTheme: TextTheme(
+      headline4: defaultTheme.headline4?.copyWith(color: CupertinoColors.inactiveGray, fontSize: 30),
+      headline3: defaultTheme.headline3?.copyWith(color: CupertinoColors.inactiveGray, fontSize: 40),
+      headline1: defaultTheme.headline1?.copyWith(color: Colors.black, fontSize: 100),
+    ),
+    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.black),
+  );
 }
